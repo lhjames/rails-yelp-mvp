0f7f33f88adb8104b04ac6e81ee5ec42f66486f1
Lucille James <lucille.james00@gmail.com>
2020-05-13 15:19:55 +0200

    added the restaurants and reviews features

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_092202) do
+ActiveRecord::Schema.define(version: 2020_05_13_092553) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.text "address"
-    t.integer "phone"
+    t.string "address"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
